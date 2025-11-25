0481f5864a5c4d7f7e62d83c574bcfbb2f061157
Karvsky <karvsky@gmail.com>
2025-11-25 16:09:53 +0100

    I added cnns_model

---
+++ model.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def cnns_model():
+    model = tf.keras.models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        layers.MaxPooling2D(2, 2),
+        layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        layers.MaxPooling2D(2, 2),
+        layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+
+        layers.Flatten(),
+        layers.Dense(256, activation='relu'),
+        layers.BatchNormalization(),
+        layers.Dense(256, activation='relu'),
+        layers.Dropout(0.1),
+        layers.BatchNormalization(),
+        layers.Dense(256, activation='relu'),
+        layers.Dropout(0.2),
+        layers.BatchNormalization(),
+        layers.Dense(256, activation='relu'),
+        layers.Dropout(0.2),
+        layers.BatchNormalization(),
+        layers.Dense(256, activation='relu'),
+        layers.Dropout(0.1),
+        layers.BatchNormalization(),
+        layers.Dense(1, activation='sigmoid')
+    ])
+
+    return model
